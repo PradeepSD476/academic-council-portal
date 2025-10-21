91e76b86f41f7e98e80639d9841af96856a14855
PradeepSD476 <sagitrapradeep2006@gmail.com>
2025-10-21 10:28:40 +0000

    we will be using Hanuman font consistently throughout the site

--- client/src/pages/Home/page.jsx
+++ client/src/pages/Home/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='tablet:bg-red-900 laptop:bg-amber-200 desktop:bg-green-700'>
+    <div className='font-hanuman'>
       home
     </div>
   )
