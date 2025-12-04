ae1b68a4ef0832e16ec0883f9457abc09e08bf36
PradeepSD476 <sagitrapradeep2006@gmail.com>
2025-12-04 18:00:25 +0000

    resource: CRUD, course: CR, Verified Via Postman

--- server/utils/extractDetails.js
+++ server/utils/extractDetails.js
@@ -1,5 +1,30 @@
-const details = (email) => {
-    const rollNo = email.split('@')[0].split('_')[1];;
-    console.log(rollNo);
-}
+export const parseRollNumber = (rollNo) => {
+  if (!rollNo || rollNo.length !== 8) {
+    return { valid: false, error: "Invalid roll number length" };
+  }
 
+  const rollUpper = rollNo.toUpperCase();
+  
+  const yearCode = rollUpper.substring(0, 2);
+  const programCode = rollUpper.substring(2, 4);
+  const branchCode = rollUpper.substring(4, 6);
+  
+  const admissionYear = 2000 + parseInt(yearCode);
+
+  const programMap = {
+    '01': 'BTECH',
+    '02': 'BTECH + MTECH',
+    '03': 'BTECH + MBA'
+  };
+
+  const program = programMap[programCode] || 'UP';
+  const branchName = branchCode;
+
+  return {
+    valid: true,
+    rollNo: rollUpper,
+    admissionYear,
+    program,
+    branchName
+  };
+};
