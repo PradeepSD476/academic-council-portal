7da4d669e1d52ad0fd861a6a514b5a4c6da1c2d7
Vaansh <90090271+Vaansh-GitHub@users.noreply.github.com>
2026-04-18 01:52:12 +0530

    Basic Editor Done Left with Designing

--- client-acc/src/components/Editor.jsx
+++ client-acc/src/components/Editor.jsx
@@ -1,7 +1,28 @@
-const Editor=({id,title,description,experienceType,status})=>{
+import { useState } from "react"
+
+const Editor=(post)=>{
+    const {title,setTitle}=useState(post.title);
+    const {description,setDescription}=useState(post.description);
+    const {expType,setExpType}=useState(post.experienceType);
+
     return (
         <div>
-            
+            <label>Title</label>
+            <input type="text" placeholder="Title" value={title}/>
+
+            <label>Description</label>
+            <input type="textarea" placeholder="Description" value={description}/>
+
+            <label>Experience Type</label>
+            <input type="text" placeholder="Experience Type" value={expType}/>
+
+            <label>Title</label>
+            <input type="text" placeholder="Title" value={title}/>
+
+            <button>Save as Draft</button>
+            <button>Publish</button>
         </div>
     )
 }
+
+export default Editor
