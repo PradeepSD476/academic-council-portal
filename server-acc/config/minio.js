575b9ca1d8d13bcff86c6e727d70974dfa78b7be
Vaansh <90090271+Vaansh-GitHub@users.noreply.github.com>
2026-04-20 22:59:20 +0530

    Random Commit

--- server-acc/config/minio.js
+++ server-acc/config/minio.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import * as Minio from "minio";
 
+
 const storage = new Minio.Client({
   endPoint: process.env.MINIO_ENDPOINT,
   port: process.env.MINIO_PORT,
@@ -9,6 +10,7 @@ const storage = new Minio.Client({
   accessKey: process.env.MINIO_ACCESS_KEY,
   secretKey: process.env.MINIO_SECRET_KEY,
 });
+
 console.log("\x1b[34m%s\x1b[0m", "CheckPoint0/minio.js")
 
 const bucketName = process.env.MINIO_BUCKET_NAME;
@@ -17,6 +19,7 @@ if (!bucketName) {
 }
 
 async function ensureBucket() {
+  console.log("\x1b[34m%s\x1b[0m", "CheckPoint1/minio.js")
   const exists = await storage.bucketExists(bucketName);
   console.log("\x1b[34m%s\x1b[0m", "CheckPoint2/minio.js")
   if (exists) {
